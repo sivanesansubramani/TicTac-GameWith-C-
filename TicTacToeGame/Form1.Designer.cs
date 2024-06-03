fb60c522799c197d4512f5e4d23c4d25f9315c5c
SivanesanSubramani <sivanesansubramaniu.s@gmail.com>
2024-06-03 17:18:30 +0530

    after tested

---
+++ TicTacToeGame/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace TicTacToeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Location = new System.Drawing.Point(12, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(300, 300);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.newGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGameButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.newGameButton.Location = new System.Drawing.Point(96, 318);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(125, 32);
+            this.newGameButton.TabIndex = 1;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = false;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(324, 353);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Panel gamePanel;
+    }
+}
+
---
+++ TicTacToeGame/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeGame
+{
+    public partial class Form1 : Form
+    {
+        private const int BoardSize = 3;
+        private const int CellSize = 100;
+        private char[,] board;
+        private char currentPlayer;
+        private bool gameOver;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBoard();
+            MaximizeBox = false;
+        }
+
+        private void InitializeBoard()
+        {
+            board = new char[BoardSize, BoardSize];
+            currentPlayer = 'X';
+            gameOver = false;
+
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+
+            gamePanel.Controls.Clear();
+            gamePanel.Refresh();
+
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    Button button = new Button
+                    {
+                        Size = new Size(CellSize, CellSize),
+                        Location = new Point(col * CellSize, row * CellSize),
+                        Font = new Font("Arial", 36),
+                        Tag = new Point(row, col)
+                    };
+                    button.Click += Button_Click;
+                    gamePanel.Controls.Add(button);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+                return;
+
+            Button button = (Button)sender;
+            Point position = (Point)button.Tag;
+            int row = position.X;
+            int col = position.Y;
+
+            // Check if the selected cell is empty
+            if (board[row, col] == ' ')
+            {
+                board[row, col] = currentPlayer;
+                button.Text = currentPlayer.ToString();
+
+                // Check if the current player has won
+                if (HasPlayerWon(currentPlayer))
+                {
+                    MessageBox.Show($"Player {currentPlayer} wins!");
+                    gameOver = true;
+                }
+                else if (IsBoardFull())
+                {
+                    MessageBox.Show("It's a draw!");
+                    gameOver = true;
+                }
+                else
+                {
+                    // Switch to the next player
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+
+        private bool HasPlayerWon(char player)
+        {
+            // Check rows
+            for (int row = 0; row < BoardSize; row++)
+            {
+                if (board[row, 0] == player && board[row, 1] == player && board[row, 2] == player)
+                    return true;
+            }
+
+            // Check columns
+            for (int col = 0; col < BoardSize; col++)
+            {
+                if (board[0, col] == player && board[1, col] == player && board[2, col] == player)
+                    return true;
+            }
+
+            // Check diagonals
+            if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+                return true;
+
+            if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
+                return true;
+
+            return false;
+        }
+
+        private bool IsBoardFull()
+        {
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    if (board[row, col] == ' ')
+                        return false;
+                }
+            }
+            return true;
+        }
+
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            InitializeBoard();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
